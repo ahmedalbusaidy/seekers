a48e576b077848616d0622fda73b0cff49ffde05
amiralic <amiralic@students.cs.ubc.ca>
2024-12-01 04:07:48 -0800

    the most trash code in our lifetimes but at least the camera doesn't go inside walls and shit

--- src/systems/CameraSystem.hpp
+++ src/systems/CameraSystem.hpp
@@ -10,6 +10,44 @@ namespace CameraSystem {
 
         glm::vec2 desired_camera_pos = player_motion.position - (player_dir * 3.0f) + (1.2f * ortho_player_dir);
 
+        for (auto& e : registry.near_cameras.entities) {
+            if (registry.walls.has(e) || (registry.static_objects.has(e) && registry.static_objects.get(e).type == STATIC_OBJECT_TYPE::PORTAL)) {
+                auto& pos = registry.motions.get(e).position;
+                auto& bound = registry.collision_bounds.get(e).wall->aabb;
+                glm::vec2 min = pos + bound.min;
+                glm::vec2 max = pos + bound.max;
+                bool done = false;
+                for (int i = 30; i >= 0; i--) {
+                    float portion = i / 30.0f;
+                    glm::vec2 point = (player_motion.position - desired_camera_pos) * portion + desired_camera_pos;
+                    while (point.x > min.x && point.x < max.x &&
+                        point.y > min.y && point.y < max.y) {
+                        desired_camera_pos = point + player_dir * 0.1f;
+                        done = true;
+                        break;
+                        }
+                    if (done) break;
+                }
+            } else if (registry.static_objects.has(e)) {
+                auto& static_obj = registry.static_objects.get(e);
+                if (static_obj.type == STATIC_OBJECT_TYPE::TREE || static_obj.type == STATIC_OBJECT_TYPE::ROCK) { // add other stuff you want camera collision for
+                    auto& pos = registry.motions.get(e).position;
+                    float radius = registry.collision_bounds.get(e).circle.radius;
+                    bool done = false;
+                    for (int i = 30; i >= 0; i--) {
+                        float portion = i / 30.0f;
+                        glm::vec2 point = (player_motion.position - desired_camera_pos) * portion + desired_camera_pos;
+                        while (glm::distance(point, pos) < radius) {
+                            desired_camera_pos = point + player_dir * 0.1f;
+                            done = true;
+                            break;
+                            }
+                        if (done) break;
+                    }
+                }
+            }
+        }
+
         Globals::desired_camera_position = desired_camera_pos;
     }
 };
